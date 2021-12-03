ae728e6401bfd23d7b9acba07e71225bb92f8789
Giuseppe Corsino <giuseppecorsino97@gmail.com>
2021-12-03 23:10:45 +0100

    FEAT: add app entrypoint

--- app/App.php
+++ app/App.php
@@ -2,13 +2,27 @@
 
 namespace App;
 
+use App\Providers\AppRouteProvider;
+use CorsDev\SimpleFrameworkCore\constants\HTTPStatusCodes;
+use CorsDev\SimpleFrameworkCore\Response\Response;
+
 class App
 {
 
-    public function boot(): void
+    public static function boot(): void
     {
+        try {
 
+            $provider = new AppRouteProvider();
+            $provider->handleRequest();
 
+        } catch (\Exception $e) {
+
+            Response::make()->statusCode(HTTPStatusCodes::INTERNAL_SERVER_ERROR)->jsonBody([
+                'error' => $e->getMessage(), 'stacktrace' => $e->getTrace()
+            ]);
+
+        }
 
     }
 
