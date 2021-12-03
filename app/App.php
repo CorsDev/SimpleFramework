69b0cb3c78a4f70488c0accbb845a95a4ea20073
Giuseppe Corsino <giuseppecorsino97@gmail.com>
2021-12-03 23:23:35 +0100

    CHORE: exception response

--- app/App.php
+++ app/App.php
@@ -3,7 +3,6 @@
 namespace App;
 
 use App\Providers\AppRouteProvider;
-use CorsDev\SimpleFrameworkCore\constants\HTTPStatusCodes;
 use CorsDev\SimpleFrameworkCore\Response\Response;
 
 class App
@@ -17,11 +16,7 @@ class App
             $provider->handleRequest();
 
         } catch (\Exception $e) {
-
-            Response::make()->statusCode(HTTPStatusCodes::INTERNAL_SERVER_ERROR)->jsonBody([
-                'error' => $e->getMessage(), 'stacktrace' => $e->getTrace()
-            ])->sendAndDie();
-
+            Response::fromException($e)->sendAndDie();
         }
 
     }
